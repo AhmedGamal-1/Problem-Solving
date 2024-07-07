ec4b8a2b8807bc4cd53a9368be8b7961ed0375a2
Ahmed Gamal <63426707+AhmedGamal-1@users.noreply.github.com>
2024-07-07 20:07:15 +0300

    Valid parantheses

--- valid-parentheses/script.js
+++ valid-parentheses/script.js
@@ -12,24 +12,44 @@ Every close bracket has a corresponding open bracket of the same type.
 
 var isValid = function (s) {
     const stack = [];
-    const template = "() [] {}";
-
-    let i = 0;
-
-    while (i < s.length) {
-        stack.push(s[i]);
-        i++;
-
-        const open = stack.at(-2);
-        const close = stack.at(-1);
-
-        const fullParanthes = open + close;
-
-        if (template.includes(fullParanthes)) {
-            stack.pop();
-            stack.pop();
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
+            stack.push(s[i])
+        } else {
+            let prevChar = stack.pop();
+            if (prevChar == '(' && s[i] != ')') return false;
+            if (prevChar == '[' && s[i] != ']') return false;
+            if (prevChar == '{' && s[i] != '}') return false;
+            if (prevChar == undefined) return false;
         }
-
     }
     return stack.length == 0;
 };
+
+
+
+// Another solution
+
+// var isValid = function (s) {
+//     const stack = [];
+//     const template = "() [] {}";
+
+//     let i = 0;
+
+//     while (i < s.length) {
+//         stack.push(s[i]);
+//         i++;
+
+//         const open = stack.at(-2);
+//         const close = stack.at(-1);
+
+//         const fullParanthes = open + close;
+
+//         if (template.includes(fullParanthes)) {
+//             stack.pop();
+//             stack.pop();
+//         }
+
+//     }
+//     return stack.length == 0;
+// };
